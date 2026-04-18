4d342382987049ea0992d097fe907746bb8f9a93
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-04-18 19:01:13 +0200

    NavBar left side, gonna have logo for now

--- Application/React_Frontend/src/Navbar.tsx
+++ Application/React_Frontend/src/Navbar.tsx
@@ -4,16 +4,15 @@ import './Navbar.css'
 const Navbar = () => {
   return (
 
-<nav className="navbar">    
-    {
+<nav className="navbar">
+  <div className= "navbar-left">
+    <a href="/" className="logo">   
+    AisMapDanmarkLogo
+    </a>
+  </div>
 
+  
 
-
-
-
-
-
-    }
 
 
 
