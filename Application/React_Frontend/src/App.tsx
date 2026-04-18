b5b76d623b28308a321f057441856dfabdf70cfb
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-04-18 23:57:40 +0200

    Added the Navbar element to the App.tsx! It's on the screen now!

--- Application/React_Frontend/src/App.tsx
+++ Application/React_Frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+
+import Navbar from './Navbar';
 import './App.css'
 
 function App() {
@@ -17,6 +19,10 @@ function App() {
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <Navbar></Navbar>
+             <h1>NEW STUFF INC</h1>
         </div>
         <div>
           <h1>Welcome to AIS_Map_Denmark</h1>
