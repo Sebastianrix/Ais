4d47533128e4fe066bce206418e1150818c8e4fd
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-04-20 00:17:58 +0200

    Added About page to Router! We use React v6 Routers. Works good.

--- Application/React_Frontend/src/main.tsx
+++ Application/React_Frontend/src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import About from './pages/About.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+//  <StrictMode>
+  //  <App />
+ // </StrictMode>,
+
+<StrictMode>
+  <BrowserRouter>
+    <Routes>  
+      <Route path = "/" element={<App />} />
+      <Route path = "/About" element={<About />} />
+    </Routes>
+  </BrowserRouter>
+</StrictMode>
+
+
+,
 )
