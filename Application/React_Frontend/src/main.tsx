a4a885ba5ba70de91b7552393c2a08ccfb15d1da
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-04-20 00:37:52 +0200

    Full router implanted! Now you can use navigate between the pages fully impleneted!

--- Application/React_Frontend/src/main.tsx
+++ Application/React_Frontend/src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App.tsx'
 import About from './pages/About.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import FAQ from './pages/FAQ.tsx'
+import Landing from './pages/Landing.tsx'
+import Map from './pages/Map.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
 //  <StrictMode>
@@ -15,6 +19,9 @@ createRoot(document.getElementById('root')!).render(
     <Routes>  
       <Route path = "/" element={<App />} />
       <Route path = "/About" element={<About />} />
+      <Route path = "/FAQ" element={<FAQ />} />
+      <Route path = "/Landing" element={<Landing />} />
+      <Route path = "/Map" element={<Map />} />
     </Routes>
   </BrowserRouter>
 </StrictMode>
