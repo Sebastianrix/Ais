fc12ab1b77477e288b86ca1248f05e1b0b9122c0
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-04-27 17:40:03 +0200

    DataLayer + Dependencies +.csproj FIXED!!

--- Application/dotNET/DataLayer/obj/Debug/net8.0/DataLayer.AssemblyInfo.cs
+++ Application/dotNET/DataLayer/obj/Debug/net8.0/DataLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d619a160f51fbc7fc9dcf697b1ff3b3493d381a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2dbd1d44f7eac0ce5cd8c327a339a5d128b670d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
