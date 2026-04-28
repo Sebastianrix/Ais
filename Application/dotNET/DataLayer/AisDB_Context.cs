ec72fb9c257e70f379ffa51f9d52d0cbd9730ad9
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-04-28 07:13:25 +0200

    Mapping, DTO, Model for TankerPosition

--- Application/dotNET/DataLayer/AisDB_Context.cs
+++ Application/dotNET/DataLayer/AisDB_Context.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
@@ -47,6 +49,8 @@ namespace DataLayer
 
 
         public DbSet<TankerPosition> TankerPositions { get; set; }
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             MapTankerPositions(modelBuilder);
@@ -56,9 +60,37 @@ namespace DataLayer
             MapVoyages(modelBuilder);
         }
 
-        private static void MapTankerPositions(ModelBuilder modelBuilder){
-
+        private static void MapTankerPositions(ModelBuilder modelBuilder)
+        {
+        modelBuilder.Entity<TankerPosition>().ToTable("tanker_positions");
+        modelBuilder.Entity<TankerPosition>().HasKey(tp => tp.Position_Id);
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Position_Id).HasColumnName("position_id");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Tanker_Id).HasColumnName("tanker_id");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Voyage_Id).HasColumnName("voyage_id");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Staging_Id).HasColumnName("staging_id");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Timestamp).HasColumnName("timestamp");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Lantitude).HasColumnName("lantitude");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Latitude).HasColumnName("latitude");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Raw_Imo).HasColumnName("raw_imo");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Imo_Status).HasColumnName("imo_status");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Raw_Mmsi).HasColumnName("raw_mmsi");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Mmsi_Status).HasColumnName("mmsi_status");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Anomaly_Flag).HasColumnName("anomaly_flag");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Navigational_Status).HasColumnName("navigational_status");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Rot).HasColumnName("rot");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Sog).HasColumnName("sog");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Cog).HasColumnName("cog");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Heading).HasColumnName("heading");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Draught).HasColumnName("draught");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Destination).HasColumnName("destination");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Eta).HasColumnName("eta");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Position_Fixing_Device).HasColumnName("position_fixing_device");
+        modelBuilder.Entity<TankerPosition>().Property(tp => tp.Data_Source_Type).HasColumnName("data_source_type");
+        
+        
         }
+        
+        
         private static void MapTankerStaging(ModelBuilder modelBuilder){}
         private static void MapTankers(ModelBuilder modelBuilder){}
         private static void MapTrackedTankers(ModelBuilder modelBuilder){}
--- Application/dotNET/DataLayer/Models/TankerPosition.cs
+++ Application/dotNET/DataLayer/Models/TankerPosition.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataLayer.Models
 {
-    internal class TankerPosition
+    public class TankerPosition
     {
+        [Required]
+        public int Position_Id { get; set; }
+        [Required]
+        public int Tanker_Id { get; set; }
+        public int Voyage_Id { get; set; }
+        public int Staging_Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public double Lantitude { get; set; }
+        public double Latitude { get; set; }
+        public string Raw_Imo { get; set; }
+        public string Imo_Status { get; set; }
+        public string Raw_Mmsi { get; set; }
+        public string Mmsi_Status { get; set; }
+        public Boolean Anomaly_Flag { get; set; }
+        public string? Navigational_Status { get; set; }
+        public double? Rot { get; set; }
+        public double? Sog { get; set; }
+        public double? Cog { get; set; }
+        public double? Heading { get; set; }
+        public double? Draught { get; set; }
+        public string? Destination { get; set; }
+        public DateTime Eta { get; set; }
+        public string? Position_Fixing_Device { get; set; }
+        public string Data_Source_Type { get; set; }
+        [Required]
+        public DateTime Created_At { get; set; }
     }
 }
