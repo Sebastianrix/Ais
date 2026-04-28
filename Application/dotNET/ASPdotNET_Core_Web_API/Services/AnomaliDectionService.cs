6c8bd4962b6d654f57d2fc4b736d6f02f393202f
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-04-28 04:23:02 +0200

    This is necessary later for verifying IMO and MMSI

---
+++ Application/dotNET/ASPdotNET_Core_Web_API/Services/AnomaliDectionService.cs
@@ -0,0 +1,18 @@
+﻿/*
+namespace ASP.NET_Core_Web_API.Services
+{
+    public class AnomaliDectionService
+    {
+        public VerifyMmsi(DTOs.RawMmsi) {
+            if (int.TryParse(DTOs.RawMmsi)) {
+                                             
+                // Verified, should we transmute to Int?
+        }
+            else { 
+            
+              // Anomali! it's a string, we should log this 
+            
+            }
+    }
+}
+    */
