083ac10aac5ff82c01558b4c53cecfd042536273
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-04-28 08:18:36 +0200

    I wanted to finish for a Endpoint, but Im falling asleep.

---
+++ Application/dotNET/ASPdotNET_Core_Web_API/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Web_API.Controllers
+
+{
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private readonly LinkGenerator _linkGenerator;
+
+        private BaseController(LinkGenerator linkGenerator)
+        {
+            _linkGenerator = linkGenerator;
+        }
+        protected string? GetUrl(string linkName, object args)
+        {
+            var uri = _linkGenerator.GetUriByName(HttpContext, linkName, args);
+            return uri;
+        }
+    }
+}
---
+++ Application/dotNET/ASPdotNET_Core_Web_API/Controllers/TankerPositionsController.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace ASP.NET_Core_Web_API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class TankerPositionsController : BaseController
+    {
+        private readonly IDataService _dataService;
+
+        public TankerPositionsController(IDataService dataService, LinkGenerator linkGenerator)
+            : base(linkGenerator)
+        {
+            _dataService = dataService; 
+        } 
+    
+    }
+}
