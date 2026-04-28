92825d6663f3922dc06dcd4b95c68e1106f16346
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-04-28 07:46:32 +0200

    IDataService/DataService for TankerPositions

--- Application/dotNET/DataLayer/DataService.cs
+++ Application/dotNET/DataLayer/DataService.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DataLayer
 {
-    internal class DataService
+    public class DataService : IDataService
     {
+        private readonly AisDB_Context _context;
 
+        public DataService(IConfiguration configuration)
+        {
+            var options = new DbContextOptionsBuilder<AisDB_Context>().UseNpgsql(configuration.GetConnectionString("aisDatabase")).Options;
+            _context = new AisDB_Context(options);
+        }
+
+        // private IList<T> GetPagedResults   *Implement later*
+
+        public IList<TankerPosition> GetTankerPositions() {
+        return _context.TankerPositions
+                .OrderByDescending(tp => tp.Timestamp)
+                .Take(100) // Remove this after Paging, This hack just make the program not freeze, since otherwise SQL would return thusands of rows.
+                .ToList();
+        }
+      
     }
 }
--- Application/dotNET/DataLayer/IDataService.cs
+++ Application/dotNET/DataLayer/IDataService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataLayer
 {
-    internal interface IDataService
+    public interface IDataService
     {
+        IList<TankerPosition> GetTankerPositions();
     }
 }
