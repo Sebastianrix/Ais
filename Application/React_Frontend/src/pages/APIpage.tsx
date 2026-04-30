ffd63f432c9da1751e2028ec0fd596245be5667d
Sebastian Rix <82590812+Sebastianrix@users.noreply.github.com>
2026-05-01 01:58:33 +0200

    added new api endpoints to frontend api documentation/new page text

--- Application/React_Frontend/src/pages/APIpage.tsx
+++ Application/React_Frontend/src/pages/APIpage.tsx
@@ -17,12 +17,18 @@ function APIpage(){
           <p>
           Welcome to the API page. This is where you can find information about our API endpoints.
           </p>
+   
+          <p>
+          We not only provide this platform, if you are a scientist or developer, we made API freely available.
+          </p>
+
+       
         </div>
 <section id="left">
 <div>
   <ul>
-<li>https://api.aismap.dk/weatherforecast</li>
-<li>https://api.aismap.dk/swagger</li>
+<li><a href="https://api.aismap.dk/swagger">https://api.aismap.dk/swagger</a></li>
+<li><a href="https://api.aismap.dk/api/v1/TankerPositions">https://api.aismap.dk/api/v1/TankerPositions</a></li>
 <li>More coming..</li>
 
 
