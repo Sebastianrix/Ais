52adff961bcc5ff2e1a1c26bb80c58cc680a2fc1
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-05-02 12:14:08 +0200

    overload sql

--- Application/dotNET/WebLayer/Controllers/TankerStagingController.cs
+++ Application/dotNET/WebLayer/Controllers/TankerStagingController.cs
@@ -73,7 +73,7 @@ namespace WebLayer.Controllers
             catch (Exception ex)
             {
                 Console.WriteLine($"[ERR] {ex}");
-                return StatusCode(500, new
+                return StatusCode(100, new
                 {
                     message = "ERROR Tanker¨StagingController",
                     error = ex.Message
