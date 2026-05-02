0d0c7e85f968f448792319e910eb9f9965804ade
Sebastianrix <sebastianrix11@gmail.com>
2026-05-02 04:11:47 +0200

    Update TankerStagingController.cs

--- Application/dotNET/WebLayer/Controllers/TankerStagingController.cs
+++ Application/dotNET/WebLayer/Controllers/TankerStagingController.cs
@@ -1,72 +1,86 @@
-﻿//using WebLayer.DTOs;
-//using DataLayer;
-//using Microsoft.AspNetCore.Http.HttpResults;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.Controllers;
-//using Microsoft.AspNetCore.Routing;
+﻿using WebLayer.DTOs;
+using DataLayer;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Routing;
 
-//using WebLayer.DTOs;
-
-
-//namespace WebLayer.Controllers
-//{
-//    [ApiController]
-//    [Route("[controller]")]
-//    [Route("api/v1/[controller]")]
-//  public class TankerStagingController : BaseController    {
-//private readonly IDataService _dataService;
-
-//    public TankerPositionsController(IDataService dataService, LinkGenerator linkGenerator)
-//        : base(linkGenerator)
-// {        _dataService = dataService;
-//   }
+using WebLayer.DTOs;
 
 
+namespace WebLayer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TankerStagingController : BaseController
+    {
+        private readonly IDataService _dataService;
+
+        public TankerStagingController(IDataService dataService, LinkGenerator linkGenerator)
+            : base(linkGenerator)
+        {
+            _dataService = dataService;
+        }
 
 
-////    //    // GET api/v1/tankerpositions
-////    //    [HttpGet]
-////    //    public IActionResult GetTankerPositions()
-////    //    {
-////    //        try
-////    //        {
-////    //            var tankerPositions = _dataService.GetTankerPositions();
-////    //            var dto = tankerPositions.Select(tp => new TankerPositionDTO
-////    //            {
-////    //                Position_Id = tp.Position_Id,
-////    //                Tanker_Id = tp.Tanker_Id,
-////    //                Voyage_Id = tp.Voyage_Id,
-////    //                Staging_Id = tp.Staging_Id,
-////    //                Timestamp = tp.Timestamp,
-////    //                Longitude = tp.Longitude,
-////    //                Latitude = tp.Latitude,
-////    //                Raw_Imo = tp.Raw_Imo,
-////    //                Imo_Status = tp.Imo_Status,
-////    //                Raw_Mmsi = tp.Raw_Mmsi,
-////    //                Mmsi_Status = tp.Mmsi_Status,
-////    //                Anomaly_Flag = tp.Anomaly_Flag,
-////    //                Navigational_Status = tp.Navigational_Status,
-////    //                Rot = tp.Rot,
-////    //                Sog = tp.Sog,
-////    //                Cog = tp.Cog,
-////    //                Heading = tp.Heading,
-////    //                Draught = tp.Draught,
-////    //                Destination = tp.Destination,
-////    //                Eta = tp.Eta,
-////    //                Position_Fixing_Device = tp.Position_Fixing_Device,
-////    //                Data_Source_Type = tp.Data_Source_Type
-////    //            }).ToList();
 
-////             return Ok(dto);
-////    //        }
-////    //        //catch (Exception ex) {
-////    //            //return StatusCode(500, new { message = "ERROR Check if the DTO rejected the db-entry cast mismatch with datatype", error = ex.Message });
-////    //        //}
-////    //      // } 
-////    //catch (Exception ex) {
-////    //Console.WriteLine($"[ERR] {ex}"); 
-//return StatusCode(500, new { message = "ERROR Check if the DTO rejected the db-entry cast mismatch with datatype", error = ex.Message });
-////    //      }
-//}
-//}
-//}
+
+        //    GET api/TankerStaging
+        [HttpGet]
+        public IActionResult GetTankerStagings()
+        {
+            try
+            {
+                var tankerstaging = _dataService.GetTankerStagings();
+                var dto = tankerstaging.Select(ts => new TankerStagingDTO
+                {
+                  Staging_Id = ts.Staging_Id,
+                  Timestamp_Raw = ts.Timestamp_Raw,
+                  Type_Of_Mobile = ts.Type_Of_Mobile,
+        Mmsi = ts.Mmsi,
+                Latitude_Raw = ts.Latitude_Raw
+                Longitude_Raw = ts.Longitude_Raw
+                Navigational_Status = ts.Navigational_Status,
+        Rot_Raw = ts.Rot_Raw,
+        Sog_Raw = ts.Sog_Raw,
+        Cog_Raw = ts.Cog_Raw,
+
+        Heading_Raw = ts.Heading_Raw,
+        Imo = ts.Imo,
+        Callsign = ts.Callsign,
+        Vessel_Name = ts.Vessel_Name = ts.
+        Ship_Type = ts.Ship_Type,
+        Cargo_Type = ts.Cargo_Type,
+        Width_Raw  = ts.Width_Raw,
+        Length_Raw = ts.Length_Raw,
+        Position_Fixing_Device = ts.Position_Fixing_Device,
+        Draught_Raw = ts.Draught_Raw,
+        Destination = ts.Destination,
+        Eta_Raw = ts.Eta_Raw,
+        Data_Source_Type = ts.Data_Source_Type,
+        Size_A = ts.Size_A,
+         Size_B = ts.Size_B,
+            Size_C = ts.Size_C,
+                Size_D = ts.Size_D,
+                Source_File_Name = ts.Source_File_Name,
+        Source_Batch_Date  = ts.Source_Batch_Date,
+   Created_At  = ts.Created_At,
+Updated_At = ts.Updated_At
+
+    }).ToList();
+
+                return Ok(dto);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[ERR] {ex}");
+                return StatusCode(500, new
+                {
+                    message = "ERROR Tanker¨StagingController",
+                    error = ex.Message
+                });
+            }
+        }
+    }
+}
