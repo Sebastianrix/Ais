17daa6046f04768e8c23fe1032c5b5cfc8baa7e7
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-05-03 13:11:58 +0200

    Better About us info

--- Application/React_Frontend/src/pages/About.tsx
+++ Application/React_Frontend/src/pages/About.tsx
@@ -11,10 +11,43 @@ function About(){
  <Navbar></Navbar>
      
  <div>
-          <h1>About page</h1>
-          <p>
-           We are a small team of two people. Our data source is: http://aisdata.ais.dk/
-          </p>
+        <h1>About This Project</h1>
+        <ul>
+        <li>
+        <p>
+          This project is part of a Master's thesis in Computer Science at 
+          Roskilde University.
+        </p>
+        </li>
+        <li>
+        <p>
+          The system is developed by a team of two students and focuses on 
+          analyzing maritime AIS data to explore patterns in vessel movements.
+        </p>
+        </li>
+        <li>
+        <p>
+          Data is sourced from the Danish Maritime Authority:
+          http://aisdata.ais.dk/
+        </p>
+        </li>
+        <li>
+        <p>
+          The objective of the project is to investigate methods for detecting 
+          anomalous or non-transparent shipping behavior using data-driven approaches.
+        </p>
+        </li>
+        <li>
+        <p>
+          The platform is built using React, TypeScript, .NET, PostgreSQL, and Python.
+        </p>
+        </li>
+        <li>
+        <p>
+          This project is intended for research and educational purposes only.
+        </p>   
+        </li>
+        </ul>     
         </div>
 
 </>
