d6ffb497996b168a1104d6d902d99a6611f94464
Sebastian Rix <82590812+Sebastianrix@users.noreply.github.com>
2026-05-08 18:11:16 +0200

    DAG debug part 2

--- airflow/dags/ais_daily.py
+++ airflow/dags/ais_daily.py
@@ -23,11 +23,15 @@ def ais_daily():
 
         url = f"http://aisdata.ais.dk/aisdk-{target}.zip"
 
+        print(f"Downloading {url}")
+
         r = requests.get(url, timeout=300)
         r.raise_for_status()
 
         z = zipfile.ZipFile(io.BytesIO(r.content))
         csv_name = z.namelist()[0]
+
+        print(f"CSV file: {csv_name}")
 
         conn = psycopg2.connect(
             host='host.docker.internal',
@@ -38,32 +42,44 @@ def ais_daily():
         )
 
         col_map = {
-
             '# Timestamp': 'timestamp_raw',
             'Type of mobile': 'type_of_mobile',
             'MMSI': 'mmsi',
             'Latitude': 'latitude_raw',
             'Longitude': 'longitude_raw',
             'Navigational status': 'navigational_status',
-            'ROT': 'rot_raw', 'SOG': 'sog_raw', 'COG': 'cog_raw',
-            'Heading': 'heading_raw', 'IMO': 'imo', 'Callsign': 'callsign',
-            'Name': 'vessel_name', 'Ship type': 'ship_type',
-            'Cargo type': 'cargo_type', 'Width': 'width_raw',
+            'ROT': 'rot_raw',
+            'SOG': 'sog_raw',
+            'COG': 'cog_raw',
+            'Heading': 'heading_raw',
+            'IMO': 'imo',
+            'Callsign': 'callsign',
+            'Name': 'vessel_name',
+            'Ship type': 'ship_type',
+            'Cargo type': 'cargo_type',
+            'Width': 'width_raw',
             'Length': 'length_raw',
             'Type of position fixing device': 'position_fixing_device',
-            'Draught': 'draught_raw', 'Destination': 'destination',
-            'ETA': 'eta_raw', 'Data source type': 'data_source_type',
-            'A': 'size_a', 'B': 'size_b', 'C': 'size_c', 'D': 'size_d'
- }
+            'Draught': 'draught_raw',
+            'Destination': 'destination',
+            'ETA': 'eta_raw',
+            'Data source type': 'data_source_type',
+            'A': 'size_a',
+            'B': 'size_b',
+            'C': 'size_c',
+            'D': 'size_d'
+        }
 
         with z.open(csv_name) as f:
 
             for chunk in pd.read_csv(
-                              f,
-                              sep=';',
-                              chunksize=20000,
-                              low_memory=False
-                            )
+                f,
+                sep=';',
+                chunksize=20000,
+                low_memory=False
+            ):
+
+                print(f"Chunk size: {len(chunk)}")
 
                 chunk = chunk.rename(columns=col_map)
 
@@ -77,6 +93,7 @@ def ais_daily():
                 buf.seek(0)
 
                 with conn.cursor() as cur:
+
                     cur.copy_expert(
                         f"""
                         COPY tanker_staging ({','.join(chunk.columns)})
@@ -87,27 +104,27 @@ def ais_daily():
 
                 conn.commit()
 
+                print("COPY successful")
+
         conn.close()
 
+    @task()
+    def run_etl():
 
-@task()
-def run_etl():
+        hook = PostgresHook(postgres_conn_id="ais_postgres")
 
-    hook = PostgresHook(postgres_conn_id="ais_postgres")
+        conn = hook.get_conn()
 
-    conn = hook.get_conn()
+        with conn:
+            with conn.cursor() as cur:
 
-    with conn:
-        with conn.cursor() as cur:
+                with open('/opt/airflow/sql/02_Load_data.sql', 'r') as f:
+                    sql = f.read()
 
-            with open('/opt/airflow/sql/02_Load_data.sql', 'r') as f:
-                sql = f.read()
+                cur.execute(sql)
 
-            cur.execute(sql)
-
-    conn.close()
+        conn.close()
 
     download_and_stage() >> run_etl()
-
 
 ais_daily()
