ef798ef9137909adfcd651ce46b5113b58c579ef
Sebastianrix <sebastianrix11@gmail.com>
2026-05-12 06:00:20 +0200

    Datatype casting. We should middleware  or anomaly detection service on DataService

--- Application/dotNET/WebLayer/DTOs/TankerStagingDTO.cs
+++ Application/dotNET/WebLayer/DTOs/TankerStagingDTO.cs
@@ -33,10 +33,10 @@ namespace WebLayer.DTOs
         public string? Destination { get; set; }
         public string? Eta_Raw { get; set; }
         public string? Data_Source_Type { get; set; }
-        public int? Size_A { get; set; }
-        public int? Size_B { get; set; }
-        public int? Size_C { get; set; }
-        public int? Size_D { get; set; }
+        public string? Size_A { get; set; }
+        public string? Size_B { get; set; }
+        public string? Size_C { get; set; }
+        public string? Size_D { get; set; }
         //[Required]
         public string? Source_File_Name { get; set; }
         //[Required]
