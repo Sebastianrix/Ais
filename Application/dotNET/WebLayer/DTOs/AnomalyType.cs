33d5ddb438c05da170554e1bb4234ea4e3195e19
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-05-20 21:18:46 +0200

    DTO/Entity model for AnomalyType table

---
+++ Application/dotNET/WebLayer/DTOs/AnomalyType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLayer.DTOs
+{
+    internal class AnomalyType
+    {
+       public long Anomaly_Type_Id { get; set; }
+       public string Code { get; set; } = string.Empty;
+       public string Name { get; set; } = string.Empty;
+       public string? Description { get; set; }
+       public string Severity { get; set; } = string.Empty;
+       public DateTime? Created_At { get; set; }
+    }
+}
