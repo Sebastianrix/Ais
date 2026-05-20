b7c4e88a716d828abef1f02ab2a31f81a39379b5
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-05-20 21:22:17 +0200

    DTO/Entity model for DataConsumerQueue table

---
+++ Application/dotNET/DataLayer/Models/DataConsumerQueue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    internal class DataConsumerQueue
+    {
+        public long Queue_Id { get; set; }
+        public DateTime Source_Batch_Date { get; set; }
+        public int Priority { get; set; }
+        public string Requester { get; set; } = string.Empty;
+        public string Status { get; set; } = "pending";
+        public DateTime Created_At { get; set; }
+    }
+}
