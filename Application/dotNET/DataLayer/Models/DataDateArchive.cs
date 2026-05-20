cd49508d0904fcef5b005b6f0e81790bd71889d7
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-05-20 21:25:33 +0200

    DTO/Entity model for DataDateArchive table

---
+++ Application/dotNET/DataLayer/Models/DataDateArchive.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    internal class DataDateArchive
+    {
+        public DateTime Source_Batch_Date { get; set; }
+        public long Total_Rows { get; set; }
+        public long Tanker_Rows { get; set; }
+        public long Positions_Inserted { get; set; }
+        public DateTime Archived_At { get; set; }
+    }
+}
