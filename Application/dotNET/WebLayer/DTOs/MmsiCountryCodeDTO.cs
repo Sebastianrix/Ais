f20438782ae4486f33d33e880a84a12d242f0084
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-05-20 21:27:50 +0200

    DTO/Entity model for MmsiCountryCode table

---
+++ Application/dotNET/WebLayer/DTOs/MmsiCountryCodeDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLayer.DTOs
+{
+    internal class MmsiCountryCodeDTO
+    {
+        public string Mid_Code { get; set; } = string.Empty;
+        public string Country_Code { get; set; } = string.Empty;
+        public string Country_Name { get; set; } = string.Empty;
+        public string? Region { get; set; }
+        public DateTime? Created_At { get; set; }
+    }
+}
