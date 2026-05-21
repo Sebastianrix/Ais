6418217d04190ec4a2f2eee489ab0176534ff9f0
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-05-21 03:56:00 +0200

    Update DataDateArchiveController.cs

--- Application/dotNET/WebLayer/Controllers/DataDateArchiveController.cs
+++ Application/dotNET/WebLayer/Controllers/DataDateArchiveController.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using DataLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebLayer.DTOs;
 
 namespace WebLayer.Controllers
 {
-    internal class DataDateArchiveController
+    [ApiController]
+    [Route("[controller]")]
+    public class DataDateArchiveController : BaseController
     {
+        private readonly IDataService _dataService;
+
+        public DataDateArchiveController(IDataService dataService, LinkGenerator linkGenerator)
+            : base(linkGenerator)
+        {
+            _dataService = dataService;
+        }
+
+        // GET /datedatearchive
+        [HttpGet]
+        public IActionResult GetArchive()
+        {
+            try
+            {
+                var archive = _dataService.GetDataDateArchive();
+                var dto = archive.Select(a => new DataDateArchiveDTO
+                {
+                    Source_Batch_Date = a.Source_Batch_Date,
+                    Total_Rows = a.Total_Rows,
+                    Tanker_Rows = a.Tanker_Rows,
+                    Positions_Inserted = a.Positions_Inserted,
+                    Archived_At = a.Archived_At
+                }).ToList();
+                return Ok(dto);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[ERR] {ex}");
+                return StatusCode(500, new { message = "ERROR DataDateArchiveController", error = ex.Message });
+            }
+        }
     }
 }
