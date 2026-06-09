f6bdc90bb17f33e33c1902b32d9bba829de444d8
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-06-10 01:02:01 +0200

    TypeScript DTO

---
+++ Application/React_Frontend/src/types/Stats.ts
@@ -0,0 +1,27 @@
+export interface Stats {
+ TankerCount: number;
+ TrackedTankerCount: number;
+
+ TotalPositionsProcessed: number;
+ TotalStagingRowsProcessed: number;
+ DatesProcessed: number;
+ LatestBatchDa: Date;
+ OldestBatchDate: Date;
+
+ PendingBatches: Date;
+ InProgressBatches: Date;
+}
+
+       // public int TankerCount { get; set; }
+       // public int TrackedTankerCount { get; set; }
+
+
+       // public long TotalPositionsProcessed { get; set; }
+      //  public long TotalStagingRowsProcessed { get; set; }
+       // public int DatesProcessed { get; set; }
+      //  public DateTime? LatestBatchDate { get; set; }
+     //   public DateTime? OldestBatchDate { get; set; }
+
+
+   //     public int PendingBatches { get; set; }
+   //     public int InProgressBatches { get; set; }
