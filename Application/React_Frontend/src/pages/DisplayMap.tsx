8746bfc31fb9d6e795a61367e551b0c0f30dd851
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2026-06-15 18:29:44 +0200

    Updated Map Markers and Vessel pop-up with useful information and unified colors schemes

--- Application/React_Frontend/src/pages/DisplayMap.tsx
+++ Application/React_Frontend/src/pages/DisplayMap.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Card } from "@/components/ui/card";
 import { Map, MapControls, MapMarker, MarkerContent, MarkerPopup } from "@/components/ui/map";
-import '../css/DisplayMap.css';
+import styles from '../css/DisplayMap.module.css';
 import Navbar from '../components/Navbar'
 import { mapService } from "../services/MapService";
 
@@ -14,9 +14,16 @@ type Vessel = {
   is_Anomalous: boolean;
 };
 
-function DisplayMap() {
-  const [vessels, setVessels] = useState<Vessel[]>([]);+
 
+
+
+
+
+
+
+function DisplayMap() {
+  const [vessels, setVessels] = useState<Vessel[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
 
 useEffect(() => {
@@ -29,26 +36,46 @@ useEffect(() => {
   return (
     <>
       <Navbar />
-      <div className='myMapClass1'>
+      <div className={styles.mapContainer}>
         <Card className="p-0 rounded-none ring-0 border-0 w-[850px] h-[700px] overflow-hidden">
           <Map center={[11.0, 56.0]} zoom={6} loading={loading}>
             <MapControls position="top-left" />
             {vessels.map(v => (
               <MapMarker key={v.tanker_Id} longitude={v.longitude} latitude={v.latitude}>
                 <MarkerContent>
-                  <div
-                    className="h-3 w-3 rounded-full border-2 border-white shadow"
-                    style={{ background: v.is_Anomalous ? "#ef4444" : "#3b82f6" }}
-                  />
-                </MarkerContent>
-                <MarkerPopup closeButton>
-                  <strong>{v.vessel_Name ?? "Unknown"}</strong><br />
-                  Flag: {v.flag} · {v.ship_Type}<br />
-                  SOG: {v.sog ?? "—"} kn · Heading: {v.heading ?? "—"}°<br />
-                  {v.is_Anomalous && <span style={{color:"#ef4444"}}>⚠ Flagged (invalid MMSI)</span>}
-                </MarkerPopup>
-              </MapMarker>
-            ))}
+                  <div className={styles.markerCircle}
+    style={{
+      width: "22px",
+      height: "22px",
+      borderColor: v.is_Anomalous ? "#ef4444" : "#3b82f6",
+      fontSize: "12px",
+    }}
+  >
+    {v.flag}
+  </div>
+      </MarkerContent>
+          <MarkerPopup className={styles.popUp}  closeButton>
+              <strong>({v.flag}) {v.vessel_Name ?? "Unknown"}</strong>
+              <br />
+              Flag: (v.flag) {v.flag}
+              <br />
+              Type: {v.ship_Type}
+              <br />
+              SOG: {v.sog ?? "—"} kn 
+              <br />
+              Heading: {v.heading ?? "—"}°
+              <br />
+              Date: {v.timestamp_Utc.split("T")[0]}
+              <br />
+              Hour: {v.timestamp_Utc.split("T")[1]}
+              {v.is_Anomalous && (
+              <span className={styles.anomalyText}>
+              Flagged (invalid MMSI)
+              </span>
+              )}
+              </MarkerPopup>
+          </MapMarker>
+              ))}
           </Map>
         </Card>
       </div>
